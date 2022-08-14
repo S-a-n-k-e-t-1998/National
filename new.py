8a561dcaa3834d47d6e6ebf370492b718c45e42a
Sanket Bodake <sanketbodake451998@gmail.com>
2022-08-14 13:35:33 +0530

    add all file

---
+++ new.py
@@ -0,0 +1,15 @@
+# from crypt import methods
+from flask import Flask,render_template
+
+
+app=Flask(__name__)
+
+
+@app.route('/')
+def Home():
+    return render_template('filter.html')
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
